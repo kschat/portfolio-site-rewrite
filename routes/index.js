081a0da7e3781e2cbc306f4eabf9c20fe2915ba5
kschat <kyleschattler@gmail.com>
2013-11-18 18:49:52 -0500

    Added view partials for each page.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
+var fs = require('fs');
+
 exports.init = function init(app) {
-  app.get(/^\/(?:about|projects|resume|blog)?$/, function(req, res) {
-  	res.render('index', { env: app.get('env').toUpperCase() });
-  });
+	app.get(/^\/(about|projects|resume|blog)?$/, function(req, res) {
+		var content = fs.readFileSync('views/' + (req.params[0] || 'about') + '.ejs', 'utf-8');
+
+		if(req.xhr) {
+			res.send({ content: content });
+		}
+		else {
+			res.render('index', { content: content, env: app.get('env').toUpperCase() });
+		}
+	});
 };
